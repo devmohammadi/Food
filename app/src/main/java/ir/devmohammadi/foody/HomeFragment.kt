94c0135493a0c5bec1984362c8cfeea240ef5529
Fatemeh Mohammadi <58948784+devmohammadi@users.noreply.github.com>
2023-08-01 10:30:29 +0330

    Set ShimmerRecyclerView

--- app/src/main/java/ir/devmohammadi/foody/HomeFragment.kt
+++ app/src/main/java/ir/devmohammadi/foody/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.facebook.shimmer.ShimmerFrameLayout
+import com.todkars.shimmer.ShimmerRecyclerView
 
 class HomeFragment : Fragment() {
 
@@ -13,6 +15,11 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        val recyclerView = view.findViewById<ShimmerRecyclerView>(R.id.recyclerView)
+        recyclerView.showShimmer()
+
+        return view
     }
 }
